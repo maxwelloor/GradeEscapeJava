1590588acdd7037fb2a9987dbf809812c750ac1b
maxwelloor <oortmax@gmail.com>
2019-11-16 13:47:26 -0500

    Started flashlight class.

--- src/dev/java2dgame/states/GameState.java
+++ src/dev/java2dgame/states/GameState.java
@@ -2,7 +2,7 @@ package dev.java2dgame.states;
 
 import java.awt.Graphics;
 
-import dev.java2dgame.entities.creatures.Player;
+import dev.java2dgame.gfx.Flashlight;
 import dev.java2dgame.main.Handler;
 import dev.java2dgame.ui.SprintBarUI;
 import dev.java2dgame.ui.UIManager;
@@ -11,12 +11,13 @@ import dev.java2dgame.worlds.World;
 public class GameState extends State{
 	
 	private UIManager uiManager;
+	private Flashlight flashlight;
 	
 	public GameState(Handler handler) {
 		super(handler);
 		handler.setWorld(new World(handler, "1"));
 		
-		
+		flashlight = new Flashlight(handler);
 		uiManager = new UIManager(handler);
 		handler.getMouseManager().setUiManager(uiManager);
 		handler.getMouseManager().getUiManager().addObject(new SprintBarUI(handler));
@@ -31,6 +32,7 @@ public class GameState extends State{
 	@Override
 	public void render(Graphics g) {
 		handler.getWorld().render(g);
+		flashlight.render(g);
 		uiManager.render(g);
 	}
 }
